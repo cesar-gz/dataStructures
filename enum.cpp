054cad2f78b88a04da82352910c5080d598e312d
cesar-gz <cesargutierrezceja@gmail.com>
2022-11-02 15:07:13 -0700

    practiced learnign how to use enumerations with a deck of cards.

---
+++ enum.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// enumerations are a collection of posssible values something can be
+enum CardSuits{
+    Club, Diamond, Heart, Spade
+};
+
+enum CardElements{
+    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine,
+    Ten, Jack, Queen, King
+};
+
+string GetSuitString(CardSuits suit){
+    string s;
+    switch(suit){
+        case Club:
+            s = "Clubs";
+            break;
+        case Diamond:
+            s = "Diamonds";
+            break;
+        case Heart:
+            s = "Hearts";
+            break;
+        case Spade:
+            s = "Spades";
+            break;
+    }
+    return s;
+}
+
+string GetElementString(CardElements element){
+    string e;
+    switch(element){
+        case Ace:
+            e = "Ace";
+            break;
+        case Two:
+            e = "Two";
+            break;
+        case Three:
+            e = "Three";
+            break;
+        case Four:
+            e = "Four";
+            break;
+        case Five:
+            e = "Five";
+            break;
+        case Six:
+            e = "Six";
+            break;
+        case Seven:
+            e = "Seven";
+            break;
+        case Eight:
+            e = "Eight";
+            break;
+        case Nine:
+            e = "Nine";
+            break;
+        case Ten:
+            e = "Ten";
+            break;
+        case Jack:
+            e = "Jack";
+            break;
+        case Queen:
+            e = "Queen";
+            break;
+        case King:
+            e = "King";
+            break;
+    }
+    return e;
+}
+
+int GenerateRandomNumber(int min, int max){
+    // static is used so we only calculate this value once
+    static const double fraction = 1.0/(static_cast<double>(RAND_MAX) + 1.0);
+    // evenlt distribute the random number across our range
+    return min + static_cast<int>(
+        (max - min + 1) * (rand() * fraction)
+    );
+}
+
+int main(){
+    // set the initial seed value to system clock
+    srand(static_cast<unsigned int>(time(0)));
+
+    // generate random suit and element card
+    int iSuit = GenerateRandomNumber(0,3);
+    int iElement = GenerateRandomNumber(0,12);
+
+    CardSuits suit = static_cast<CardSuits>(iSuit);
+    CardElements element = static_cast<CardElements>(iElement);
+
+    cout << "Your card is the "; 
+    cout << GetElementString(element);
+    cout << " of " << GetSuitString(suit) << endl;
+    return 0;
+}
